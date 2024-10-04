e5e48408123a8718806d74539800c020fe8f0de4
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-10-04 09:55:50 +0200

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "ez a 'hello world' controller 'index' actionja";
+        }
+
+        public string Welcome(string name, int age = 14)
+        {
+            return $"Szia, {name} {((age < 18) ? "mennyé' haza, ez nem neked való" : "ingyen cukorka a tranzitban")}";
+        }
+    }
+}
